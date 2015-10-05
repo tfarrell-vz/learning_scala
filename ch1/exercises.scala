2f178005dfac8fdf0d83aa84e5e46b88758ac85c
tfarrell-vz <farrell@abiresearch.com>
2015-10-04 20:58:41 -0400

    Exercise 1

---
+++ ch1/exercises.scala
@@ -0,0 +1,8 @@
+/** Exercise 1
+  *
+  * An alternative to println() is print(), which does not include a trailing newline.
+  */
+
+print("This is a print not a println")
+print("Since I don't add a newline, these are joined!")
+
