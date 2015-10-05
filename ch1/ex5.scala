67c6b005d6596e44386d46ea928127683c07a201
tfarrell-vz <farrell@abiresearch.com>
2015-10-04 21:27:48 -0400

    Exercise 5. Using :paste

---
+++ ch1/ex5.scala
@@ -0,0 +1,5 @@
+/**
+  * The specified exercise instructions are incorrect. The author mean to say,
+  * "To do this, type :paste, ..."
+  * instead of :paste -raw
+  * The -raw won't work in this instance. 
