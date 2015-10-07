80c5c72777973bed5e327ff5bb58a806e6bc8c72
tfarrell-vz <farrell@abiresearch.com>
2015-10-06 21:13:33 -0400

    Formatting

--- ch1/ex5.scala
+++ ch1/ex5.scala
@@ -2,4 +2,4 @@
   * The specified exercise instructions are incorrect. The author mean to say,
   * "To do this, type :paste, ..."
   * instead of :paste -raw
-  * The -raw won't work in this instance. 
+  * The -raw won't work in this instance.
