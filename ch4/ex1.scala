7f5b36f83a03a9da08b8aaacff2d5b29994d92aa
tfarrell-vz <farrell@abiresearch.com>
2015-10-11 21:05:50 -0400

    Provide radius as a String

--- ch4/ex1.scala
+++ ch4/ex1.scala
@@ -6,3 +6,16 @@ def area(radius: Double): Double = {
   radius * radius * 3.141
 }
 
+/*
+  Exercise 2: Write an alternate form of area that takes the radius as a String.
+ */
+
+def area2(radius: String): Double = {
+  if (radius == "") {
+    0.0
+  }
+  else {
+    val r = radius.toDouble
+    r * r * 3.141
+  }
+}
