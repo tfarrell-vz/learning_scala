f902d8f35897a9022213b232c46dab3b2b8a27d5
tfarrell-vz <farrell@abiresearch.com>
2015-10-13 22:33:07 -0400

    function that takes a tuple and a function literal!

--- ch5/ex1.scala
+++ ch5/ex1.scala
@@ -1,6 +1,13 @@
 /*
   Exercise 1; Write a function literal that takes two integers and returns the higher number.
+  Then write a higher-order function that takes a 3-sized tuple of integers plus this function literal
+  to return the maximum value in the tuple.
  */
 
-val funcLiteral = (x: Int, y: Int) => if (x > y) x else y
+val funcLiteralMax = (x: Int, y: Int) => if (x > y) x else y
+
+def appliedToThree(three: (Int, Int, Int), f: (Int, Int)=>Int): Int = {
+  // Assumes that f is associative.
+  f(f(three._1, three._2), three._3)
+}
 
